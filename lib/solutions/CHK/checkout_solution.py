1413a018c1a2d9b86eda95d4924c03877eac58ed
sbx_user1051 <sbx_user1051@169.254.122.229>
2022-05-28 11:40:23 +0000

    Sat May 28 11:40:23 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,27 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+price_cat = {"A" : {"PRICE" : 50, "OFFER" : {3 : 130}}, "B" : {"PRICE" : 30, "OFFER" : {2 : 45}}, "C" : {"PRICE" : 20}, "D" : {"PRICE" : 15} }
+
 def checkout(skus):
-    raise NotImplementedError()
+    sku_list = skus.split(",")
+    sku_dict = {}
+    for sku in sku_list:
+        if not price_cat.get(sku,False):
+            return -1
+        count = sku_dict.get(sku,0) 
+        sku_dict[sku] = count +1
+    basket = 0
+    for sku in sku_dict.keys():
+        price = 0
+        item = price_cat[sku]
+        if item.get("OFFER",False):
+            quant = list(item['OFFER'].keys())[0]
+            if sku_dict[sku] % quant == 0:
+                price = (sku_dict[sku] / quant) * item['OFFER'][quant] 
+        else:
+            price = sku_dict[sku] * item['PRICE']
+        basket = basket + price
+    return basket
+
