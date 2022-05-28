dc10b8a8daa0595308f428eba85997669184cc04
sbx_user1051 <sbx_user1051@169.254.122.229>
2022-05-28 11:55:23 +0000

    Sat May 28 11:55:23 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -21,8 +21,18 @@ def checkout(skus):
             quant = list(item['OFFER'].keys())[0]
             if sku_dict[sku] % quant == 0:
                 price = (sku_dict[sku] / quant) * item['OFFER'][quant] 
+            elif sku_dict[sku] > quant:
+                i = 2
+                while sku_dict[sku] - quant * i > quant:
+                    i = i + 1
+                price = i * item['OFFER'][quant] + ((sku_dict[sku] - (quant * i))* item['PRICE'])
         else:
             price = sku_dict[sku] * item['PRICE']
         basket = basket + price
     return basket
+
+
+
+
+print(checkout("A,A"))
 
